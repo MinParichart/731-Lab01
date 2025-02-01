34c1845879f8e7a606aa28a74b451b3391061f26
MinChatr <min_etc@hotmail.com>
2025-02-01 17:22:06 +0700

    Upload compute 2 4 6

--- test.js
+++ test.js
@@ -1,5 +1,22 @@
-console.log('hello world')
+// ทดลองเขียน code if
+// let x = 10; 
+// if (x > 10) { 
+//     console.log('x is more than 10')
+// }; 
 
-let x = 10; 
-let y = 15; 
-console.log(x + y); 
+// ทดลองเขียน if-else statement
+// let x = 9 
+// if (x > 10) { 
+//     console.log('x is more than') 
+//     } else { 
+//         console.log('x is less than 10')
+//     }; 
+
+// กำาหนดคาสองค่า x > 5 และ y > 12  
+let x = 6
+let y = 13
+if (x > 5 && y > 12){ 
+    console.log("ค่ามากจัง")
+} else { 
+    console.log((x*y) + (x/y))
+}; 
