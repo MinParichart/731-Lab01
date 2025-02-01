f2b64df5458728cf98eec5b1af222d55ba5150a6
MinChatr <min_etc@hotmail.com>
2025-02-01 17:30:02 +0700

    Upload compute 8

--- test.js
+++ test.js
@@ -13,10 +13,21 @@
 //     }; 
 
 // กำาหนดคาสองค่า x > 5 และ y > 12  
-let x = 6
-let y = 13
-if (x > 5 && y > 12){ 
-    console.log("ค่ามากจัง")
+// let x = 6
+// let y = 13
+// if (x > 5 && y > 12){ 
+//     console.log("ค่ามากจัง")
+// } else { 
+//     console.log((x*y) + (x/y))
+// }
+
+// ทดลองเขียน if-else if - else statement 
+let x = 15
+if (x > 10){ 
+    console.log ('x is more than 10')
+} else if (x < 5){ 
+    console.log('x is less thatn 5')
 } else { 
-    console.log((x*y) + (x/y))
-}; 
+    console.log('x is between 5 and 10')
+}
+
