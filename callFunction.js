8302f38bf909ccd70804dc7b429a1d8b3279ed2e
MinChatr <min_etc@hotmail.com>
2025-02-01 19:45:09 +0700

    Export function2.js and Add callFunction.js

---
+++ callFunction.js
@@ -0,0 +1,3 @@
+const { multiply } = require ('./function2')
+console.log(multiply(2,3))
+console.log(multiply(5,25))
--- function2.js
+++ function2.js
@@ -9,8 +9,11 @@
 // console.log(greet('John'))
 
 // Arrow Function หลายบรรทัด 
-// const multiply = (x,y) => { return x + y }
-// console.log(multiply (5,10))
+const multiply = (x,y) => { return x + y }
+console.log(multiply (5,10))
+module.exports = { 
+    multiply
+}
 
 // Arrow Function โดยให้รับค่าตัวแปร 2 ค่า แล้วคืนค่าที่มากกว่าออกมา 
 const receiveNum = (x,y) => {
